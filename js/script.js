58e69a2243659b319ba614f95ecd9bdb9f95bf03
Eliseykin, Maxim (ADV D EU CZ AE CV 5) <maxim.eliseykin@siemens.com>
2021-11-06 12:47:43 +0100

    Films program test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+const numberOfFilms = +prompt("How many movies you watch","")
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+const lastFilm =prompt("last film what you see","");
+const awardFilm = prompt("how was good a film","");
+personalMovieDB.movies[lastFilm,awardFilm];
