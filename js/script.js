e1d3083a819d4ac1c1415cf4a639dd30b002ad3d
Eliseykin, Maxim (ADV D EU CZ AE CV 5) <maxim.eliseykin@siemens.com>
2021-11-06 15:14:26 +0100

    17

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 //const category = 'toys';
 //console.log(`https://someurl.com/${category}/5`);
 
-const numberOfFilms = +prompt("How many movies you watch","")
+const numberOfFilms = +prompt("How many movies you watch","");
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies:{},
@@ -11,8 +12,34 @@ const personalMovieDB = {
     genres:[],
     privat: false
 };
-const lastFilm = prompt("last film what you see",""),
-     awardFilm = prompt("how was good a film","");
-personalMovieDB.movies[lastFilm]=awardFilm;
+
+for (let index = 0; index < numberOfFilms; index++) {
+    
+    const lastFilm = prompt("last film what you see",""),
+          awardFilm = prompt("how was good a film","");
+        if(lastFilm != null  && awardFilm != null && lastFilm !='' && awardFilm !='' && lastFilm.length<50){
+            personalMovieDB.movies[lastFilm]=awardFilm;
+        } else{
+            index--;
+        }
+}
+
+switch (true){
+    case (personalMovieDB.count < 10): 
+        alert("wathed a low count of films"); 
+        break;
+    case (personalMovieDB.count>=10 && personalMovieDB.count0 <= 30):
+        alert("Typiccal wacher"); 
+        break; 
+    case (personalMovieDB.count>30):
+        alert("You are cinema man");
+        break;
+    default:
+        alert("Invalid data");
+        break;
+}
+
 
 console.log(personalMovieDB);
+
+
